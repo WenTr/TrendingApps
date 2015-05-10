49cd32a7d1b3f70da19ed6f90d930646914d9756
shewong <Sherry@MacBook-Pro.local>
2015-05-09 21:37:25 -0700

    Include call for screen scraping

--- testTwitterAPI.py
+++ testTwitterAPI.py
@@ -7,15 +7,23 @@ Created on Apr 25, 2015
 from json import dumps
 
 from twitterDataAcquisition import TwitterDataAcquisition
+from twitterScreenScrape import TwitterScreenScrape
 
 def main():
-    twitterData = TwitterDataAcquisition()
+    appsList = ["Messenger", "Criminal Case", "Facebook", "Pandora Radio", "Instagram", 
+            "Snapchat", "Dubsmash","Super-Bright LED Flashlight", "Spotify Music",
+            "Clean Master (Speed Booster)"]
+    
+    #twitterData = TwitterDataAcquisition()
         
     #jsonFile = ("twitter_data.json", "w")
     #jsonFile.write(str(twitterData))
     #jsonFile.close()
     
-    print dumps(twitterData)
+    #print dumps(twitterData)
+    
+    TwitterScreenScrape(appsList)
+    #TwitterScreenScrape(["Messenger"])
 
 if __name__ == '__main__':
     main()
